24ce92a4251de834abdecde7cb69c32a41ee5afb
Nagy Imre <imre.nagy@ratoong.com>
2018-01-31 15:18:58 +0100

    init

---
+++ 1st lecture.js
@@ -0,0 +1,28 @@
+// TODO: Name 7 common HTML elements.
+// div, p, a, ul, li, span, h1, b
+
+// TODO: What's the difference between the class and the id HTML attributes.
+// We use class for style-ing, id-s to reference elements. Also ID-s must be unique
+
+// TODO: What does "DOM" stand for? Explain in your own word what it refers to.
+// Document Object Model. It's the hierarchical representation of the document
+
+// TODO: What Python structure does the array correspond to?
+// list
+
+// TODO: Which Python container does object correspond to?
+// dictionary
+
+// TODO: Consider the 5 SVG squares in example 3-22. Think about how you'd
+// TODO: Add a gray boundary of width 2.
+// stroke-width="2" stroke="grey", or drawing an other grey circle behind
+
+// TODO: Make the squares increasingly transparant from left to right.
+// with opacities set
+
+// TODO: Make the squares 80 by 80 instead of 30 by 30.
+// Adjust width and height values accordingly
+
+// TODO: Make the squares go from bottom of the SVG element to the top instead of the other way around.
+// Adjust coordinates accordingly...
+
